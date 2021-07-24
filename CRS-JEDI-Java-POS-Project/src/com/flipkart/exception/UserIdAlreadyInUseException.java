92723752b2134c68bb731e362d7c0aa8f932d789
Sreenik Seal <sreenik.seal@gmail.com>
2021-07-24 16:08:06 +0530

    Add exception implementation

---
+++ CRS-JEDI-Java-POS-Project/src/com/flipkart/exception/UserIdAlreadyInUseException.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.flipkart.exception;
+
+/**
+ *
+ */
+public class UserIdAlreadyInUseException extends Exception{
+	private String userId;
+	
+	
+	public UserIdAlreadyInUseException(String userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setProfessorId(String userId) {
+		this.userId = userId;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "userId: " + userId + " is already in use.";
+	}
+
+}
