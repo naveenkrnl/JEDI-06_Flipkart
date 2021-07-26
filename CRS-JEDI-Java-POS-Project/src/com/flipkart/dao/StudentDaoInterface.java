060018d69848e5134dd77b404181c48516a4e1d2
davePranjal <pranjaldave8@gmail.com>
2021-07-26 14:51:26 +0530

    Changed Dao comments

--- CRS-JEDI-Java-POS-Project/src/com/flipkart/dao/StudentDaoInterface.java
+++ CRS-JEDI-Java-POS-Project/src/com/flipkart/dao/StudentDaoInterface.java
@@ -20,14 +20,14 @@ public interface StudentDaoInterface {
 	 * 
 	 * @param student: student object containing all the fields
 	 * @return true if student is added, else false
-	 * @throws StudentNotRegisteredException
+	 * @throws StudentNotRegisteredException If the student is not registered
 	 */
 	public int addStudent(Student student) throws StudentNotRegisteredException;
 
 	/**
 	 * Method to retrieve Student Id from User Id
 	 * 
-	 * @param userId
+	 * @param userId User Id
 	 * @return Student Id
 	 */
 	public int getStudentId(String userId);
@@ -35,8 +35,8 @@ public interface StudentDaoInterface {
 	/**
 	 * Method to check if Student is approved
 	 * 
-	 * @param studentId
-	 * @return boolean indicating if student is approved
+	 * @param studentId Student Id
+	 * @return boolean indicating if student is approved by admin
 	 */
 	public boolean isApproved(int studentId);
 }
