3a7a36d5e784ece01b08fc33351021f520e1c813
Naveen Arora <naveen.arora@BLRETV-C02FG1P0.bbrouter>
2021-07-28 22:45:04 +0530

    hotfix

--- CRS-JEDI-FLIPKART-REST/src/main/java/com/flipkart/restController/ApplicationConfig.java
+++ CRS-JEDI-FLIPKART-REST/src/main/java/com/flipkart/restController/ApplicationConfig.java
@@ -6,15 +6,10 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class ApplicationConfig extends ResourceConfig {
 
     public ApplicationConfig() {
-        // All the web servies to be registered Here
-//        register(StudentRestAPI.class);
-//        register(UserRestAPI.class);
-//        register(ProfessorRestAPI.class);
-//        register(AdminRestAPI.class);
         register(AdminRESTAPIController.class);
         register(UserRESTAPIController.class);
-
-
+        register(StudentRESTAPIController.class);
+        register(ProfessorRESTAPIController.class);
     }
 
 }
--- CRS-JEDI-FLIPKART-REST/src/main/java/com/flipkart/utils/DBUtils.java
+++ CRS-JEDI-FLIPKART-REST/src/main/java/com/flipkart/utils/DBUtils.java
@@ -34,7 +34,7 @@ public class DBUtils {
 //        password=Something@123
         DB_URL = "jdbc:mysql://localhost:3307/crs?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
         USER = "root";
-        PASS = "root";
+        PASS = "Something@123";
         try {
             System.out.println("connection");
             Class.forName("com.mysql.cj.jdbc.Driver");
